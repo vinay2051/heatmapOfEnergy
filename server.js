8f3ad081b46dc050f845b6cffee3f5363ecd526b
A.S.D.Vinay <vinayasd@MacBook-Pro.local>
2017-05-06 22:34:26 +0530

    big commit

--- server.js
+++ server.js
@@ -1,44 +1,52 @@
 // server.js
-var express        = require('express');  
-var app            = express();  
-var httpServer = require("http").createServer(app);  
-var five = require("johnny-five");  
-var io=require('socket.io')(httpServer);
- 
-var port = 3000; 
- 
+var express = require('express');
+var app = express();
+var httpServer = require("http").createServer(app);
+var five = require("johnny-five");
+var io = require('socket.io')(httpServer);
+
+var port = 3000;
+
 app.use(express.static(__dirname + '/public'));
- 
-app.get('/', function(req, res) {  
-        res.sendFile(__dirname + '/public/main.html');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/main.html');
 });
 
-httpServer.listen(port);  
-console.log('Server available at http://localhost:' + port);  
+httpServer.listen(port);
+console.log('Server available at http://localhost:' + port);
 var led;
- 
+
 //Arduino board connection
- 
-var board = new five.Board();  
-board.on("ready", function() {  
+
+var board = new five.Board();
+board.on("ready", function () {
     console.log('Arduino is ready.');
     led = new five.Led(13);
+    led.on();
 });
- 
+
 //Socket connection handler
-io.on('connection', function (socket) { 
-        console.log(socket.id);
- 
-        socket.on('led:on', function (data) {
-           led.on();
-            console.log(data);
-        });
- 
-        socket.on('led:off', function (data) {
-            led.off();
-            console.log('LED OFF RECEIVED');
- 
-        });
+io.on('connection', function (socket) {
+    console.log(socket.id);
+    socket.on('led:on', function (data) {
+        led.on();
+        console.log(data);
     });
- 
+
+    socket.on('led:off', function (data) {
+        led.off();
+        console.log('LED OFF RECEIVED');
+
+    });
+    socket.on('new', function (msg) {
+        console.log(msg);
+        socket.broadcast.emit(msg);
+    })
+    socket.on('newanalysis', function (msg) {
+        console.log(msg);
+        socket.broadcast.emit(msg);
+    })
+});
+
 console.log('Waiting for connection');
